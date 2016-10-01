64c00de7b90b2473b8d17975308fdd60160e91cd
oisdk <oisin.kidney@gmail.com>
2016-10-01 02:48:59 +0100

    point free, simplified

--- src/Hakyll/Web/Series.hs
+++ src/Hakyll/Web/Series.hs
@@ -6,11 +6,10 @@ module Hakyll.Web.Series
   ( seriesField
   , getSeries
   , buildSeries
-  , compileSeries
-  , SeriesInfo(..)
   ) where
 
 import           Control.Applicative
+import           Control.Arrow
 import           Control.Monad
 import           Data.Foldable
 import           Data.List           (elemIndex)
@@ -24,28 +23,6 @@ import           Prelude             hiding (head)
 -- except it only accepts one series per identifier.
 getSeries :: MonadMetadata m => Identifier -> m (Maybe String)
 getSeries = flip getMetadataField "series"
-
--- | Compiles all of the information available on a given series
-compileSeries :: String              -- ^ Name of series
-              -> Tags                -- ^ Collected series
-              -> Identifier          -- ^ Thing which is a member of a series
-              -> Compiler SeriesInfo
-compileSeries serie tags ident = do
-  otherPostsInSeries <- toAlt (lookup serie (tagsMap tags))
-  let seriesLen = length otherPostsInSeries
-  curInd <- toAlt (elemIndex ident otherPostsInSeries)
-  let curNum = curInd + 1
-  loc <- toAlt =<< getRoute (tagsMakeId tags serie)
-  pure $ SeriesInfo seriesLen curNum serie (toUrl loc)
-
--- | This represents the information available in an item
--- in a series, for display.
-data SeriesInfo = SeriesInfo
-  { seriesLength :: Int    -- ^ The total length of the series
-  , seriesCurPos :: Int    -- ^ The number of the current post in this series
-  , seriesName   :: String -- ^ The name of the series
-  , seriesUrl    :: String -- ^ The location of the aggregated posts
-  }
 
 toAlt :: (Foldable f, Alternative m) => f a -> m a
 toAlt = foldr ((<|>) . pure) empty
@@ -62,17 +39,39 @@ toAlt = foldr ((<|>) . pure) empty
 
 seriesField :: Tags -> Context a
 seriesField tags = Context $ const . \case
-    "series"       -> getSeriesField seriesName
-    "seriesCurPos" -> getSeriesField (show . seriesCurPos)
-    "seriesLength" -> getSeriesField (show . seriesLength)
-    "seriesUrl"    -> getSeriesField seriesUrl
-    _              -> const empty
+    "series" ->
+            seriesName
+        >>> fmap StringField
+    "seriesCurPos" ->
+            itemIdentifier &&& otherPostsInSeries
+        >>> sequence
+        >>> fmap (uncurry elemIndex)
+        >=> toAlt
+        >>> fmap (succ
+        >>> show
+        >>> StringField)
+    "seriesLength" ->
+            otherPostsInSeries
+        >>> fmap (length
+        >>> show
+        >>> StringField)
+    "seriesUrl" ->
+            seriesName
+        >=> tagsMakeId tags
+        >>> getRoute
+        >=> toAlt
+        >>> fmap (toUrl
+        >>> StringField)
+    _ -> const empty
   where
-    getSeriesField disp item = do
-      serie <- toAlt =<< getSeries ident
-      seriesInfo <- compileSeries serie tags ident
-      pure (StringField (disp seriesInfo))
-      where ident = itemIdentifier item
+    seriesName =
+            itemIdentifier
+        >>> getSeries
+        >=> toAlt
+    otherPostsInSeries =
+            seriesName
+        >=> flip lookup (tagsMap tags)
+        >>> toAlt
 
 -- | Similar to the 'buildTags' function in "Hakyll.Web.Tags", except
 -- checks the series field, and can only accept one series per item.
