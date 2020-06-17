abf54b49db5fbc06471af38e2aa7b30c392a52b3
Donnacha Oisín Kidney <oisdk@users.noreply.github.com>
2020-06-17 17:38:33 +0100

    Update Series.hs

--- src/Hakyll/Web/Series.hs
+++ src/Hakyll/Web/Series.hs
@@ -75,7 +75,7 @@ seriesField tags = Context $ const . \case
 
 -- | Similar to the 'buildTags' function in "Hakyll.Web.Tags", except
 -- checks the series field, and can only accept one series per item.
-buildSeries :: MonadMetadata m
+buildSeries :: (MonadMetadata m, MonadFail m)
             => Pattern
             -> (String -> Identifier) -- ^ Function for converting a given series name into an identifier for its page
             -> m Tags
